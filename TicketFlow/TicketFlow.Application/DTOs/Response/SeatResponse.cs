e8103d5cd640f9405e988e01e7e8aed39203dc43
Alejandro-Ariel-Salas <10alejandro4salas@gmail.com>
2026-04-18 19:59:12 -0300

    Creacion: Agregue El endpoint para traer el listado de eventos

---
+++ TicketFlow/TicketFlow.Application/DTOs/Response/SeatResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace TicketFlow.Application.DTOs.Response
+{
+    public class SeatResponse
+    {
+        public Guid Id { get; set; }
+        public string RowIdentifier { get; set; } = string.Empty;
+        public int SeatNumber { get; set; }
+        public string Status { get; set; } = string.Empty;
+    }
+}
---
+++ TicketFlow/TicketFlow.Application/Interfaces/IQuerys/IEventQuery.cs
@@ -0,0 +1,10 @@
+﻿using TicketFlow.Application.DTOs.Response;
+
+namespace TicketFlow.Application.Interfaces.IQuerys
+{
+    public interface IEventQuery
+    {
+        // Cambiamos EventDto por EventResponse
+        Task<(IEnumerable<EventResponse> Events, int TotalRecords)> GetPaginatedEventsAsync(int pageNumber, int pageSize);
+    }
+}
