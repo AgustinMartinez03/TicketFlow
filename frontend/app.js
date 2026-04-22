5f29f5fd7e110a376774ed28336ef904bf30d0e1
AgustinMartinez03 <agustincarpmartinez@gmail.com>
2026-04-22 15:42:12 -0300

    feat: inicializar estructura base del frontend con Bootstrap

---
+++ frontend/app.js
@@ -0,0 +1,5 @@
+// app.js - Lógica principal del Frontend
+console.log("¡El Frontend está vivo y conectado!");
+
+// URL base de nuestra API (ajustá el puerto al que te da tu Visual Studio)
+const API_BASE_URL = "https://localhost:5041/api/v1";
