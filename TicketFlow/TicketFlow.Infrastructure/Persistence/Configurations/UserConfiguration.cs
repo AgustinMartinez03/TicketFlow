929f77ed28c331c7644abbe8e2c620006700c1ed
AgustinMartinez03 <agustincarpmartinez@gmail.com>
2026-05-11 12:02:04 -0300

    fix(auth): corregir hashes de bcrypt en datos semilla y aplicar migracion exitosa

--- TicketFlow/TicketFlow.Infrastructure/Persistence/Configurations/UserConfiguration.cs
+++ TicketFlow/TicketFlow.Infrastructure/Persistence/Configurations/UserConfiguration.cs
@@ -32,25 +32,27 @@ namespace TicketFlow.Infrastructure.Persistence.Configurations
             builder.HasData(
                 new User
                 {
-                    Id = 3,
-                    Name = "Agustin",
-                    Email = "agus@ticketflow.com",
-                    PasswordHash = "123456"
+                    Id = 1,
+                    Name = "Admin General",
+                    Email = "admin@ticketflow.com",
+                    PasswordHash = "$2a$12$eQQOCJVOpp0NS8MJ0C5c1Oo6gbsC58uD.j6wZE9Z6xnzfMsRd1H6G",
+                    Role = "Admin"
                 },
                 new User
                 {
                     Id = 2,
                     Name = "Alejandro",
                     Email = "ale@ticketflow.com",
-                    PasswordHash = "123456"
+                    PasswordHash = "$2a$12$WQUmKUbkI6C6a2e5nDxfvO42I5fVVOnVlE1JnYAWXGP914Cs4RMPC",
+                    Role = "Client"
                 },
                 new User
                 {
-                    Id = 1,
-                    Name = "Admin General",
-                    Email = "admin@ticketflow.com",
-                    PasswordHash = "admin123",
-                    Role = "Admin"
+                    Id = 3,
+                    Name = "Agustin",
+                    Email = "agus@ticketflow.com",
+                    PasswordHash = "$2a$12$aJGDFO5wFcTycwlLZ1XfFepllyAhVEawY/pOPvmZKnUcf/BEHlDqy",
+                    Role = "Client"
                 }
             );
         }
